ae8b94bc564cb297d6a598bc2912595640fe75b7
smelik81 <artem.smelyanskiy81@gmail.com>
2024-04-25 13:33:56 +0300

    includes libreiry

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
