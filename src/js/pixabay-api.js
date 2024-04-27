926a9ed4d91359ac2ee7845014097eaacfa4e522
smelik81 <artem.smelyanskiy81@gmail.com>
2024-04-28 02:50:46 +0300

    finished main part homework task

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,21 @@
 
+export function searchPhoto(searchImages) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '43587882-3fd012595c4df59010c013d1f';
 
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: searchImages,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
 
-
-formEl.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log(inputEl.target.value);
-});
+    return fetch(`${BASE_URL}?${params}`)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error(response.status);
+            }
+            return response.json();
+  });
+}
--- src/main.js
+++ src/main.js
@@ -5,9 +5,78 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const formEl = document.querySelector('.js__search-form');
-console.log(formEl);
-const inputEl = document.querySelector('.input__form-text');
-console.log(inputEl);
-const buttonSubmit = document.querySelector('.button__form-btn');
-console.log(buttonSubmit);
+import { searchPhoto } from "./js/pixabay-api";
+import { createMarkup } from './js/render-functions';
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  sourceAttr: 'href',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+const formSearch = document.querySelector('.js__search-form');
+const list = document.querySelector('.js__list');
+const loader = document.querySelector('.hide');
+
+
+
+formSearch.addEventListener('submit', onHandleSubmit);
+function onHandleSubmit(event) {
+    event.preventDefault();
+    const { picture } = event.currentTarget.elements;
+    const searchStr = picture.value.trim();
+    loader.classList.remove('hide');
+    list.innerHTML = "";
+    if (!searchStr) {
+        iziToast.error({
+            title: 'Error',
+            message: 'Please enter details'
+        })
+        return;
+    }
+    
+    searchPhoto(picture.value)
+    
+        .then(data => {
+            if (!data.hits.length) {
+                
+                iziToast.error({
+                    title: 'Error',
+                    message:
+                        'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'bottomCenter',
+                    transitionIn: 'bounceInUp',
+                    messageColor: 'white',
+                    timeout: 3000,
+                })
+            } else {
+                setTimeout(() => {
+                    list.insertAdjacentHTML(
+                      'beforeend',
+                      createMarkup(data.hits)
+                    );
+                    lightbox.refresh();
+                    loader.classList.add('hide');
+                }, 1000)
+            }
+        })
+      
+        .catch(error => {
+           
+          iziToast.error({
+            title: 'Error',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'bottomCenter',
+            transitionIn: 'bounceInUp',
+            messageColor: 'white',
+            timeout: 3000,
+          });
+            loader.classList.add('hide')
+      })
+        .finally(() => formSearch.reset())
+    
+};
+
+
